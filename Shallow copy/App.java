1c0b5252e45126be17574d34299974360f0334d7
alishaikh04 <alishaikhzk@gmail.com>
2025-09-07 05:37:07 +0500

    Add files via upload

---
+++ Shallow copy/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+       Main m1 =  new Main();
+         m1.run();     
+    } 
+}
---
+++ Shallow copy/Array.java
@@ -0,0 +1,29 @@
+public class Array implements Cloneable{ 
+
+    private int[] anArray = {1,2,3,4,5};
+    private int values = 7;
+
+public void increment() { 
+
+    for(int i = 0; i < anArray.length; i++){
+       anArray[i]++;  
+    } 
+}
+
+public String toString() {
+   String result = "";
+   for(int i = 0; i < anArray.length; i++){
+     result += anArray[i];
+     if(i < anArray.length - 1){
+        result += ", ";
+     }
+   }
+   return result;
+}
+
+public Array clone() throws
+     CloneNotSupportedException{
+      Array cloned = (Array) super.clone();
+      return cloned;
+     } 
+}
