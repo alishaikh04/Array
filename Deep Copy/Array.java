a8fef1eee438f1140ce085f16fd52ba129375d27
alishaikh04 <alishaikhzk@gmail.com>
2025-09-07 05:39:07 +0500

    Add files via upload

---
+++ Deep Copy/Array.java
@@ -0,0 +1,32 @@
+public class Array implements Cloneable{ 
+
+    private int[] anArray = {1,2,3,4,5};
+    private int values = 7;
+
+public void increment() { 
+    int newSize = values;
+    int[] newAnArray = new int[newSize];
+    for(int i = 0; i < newSize; i++){
+        newAnArray[i] = i + 1;
+    } 
+    anArray = newAnArray;
+}
+
+public String toString() {
+   String result = "";
+   for(int i = 0; i < anArray.length; i++){
+     result += anArray[i];
+     if(i < anArray.length - 1){
+        result += ", ";
+     }
+   }
+   return result;
+}
+
+public Array clone() throws
+     CloneNotSupportedException{
+      Array cloned = (Array) super.clone();
+      cloned.anArray = anArray.clone();
+      return cloned;
+     } 
+}
