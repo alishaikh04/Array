0d4022dd5554789f6246082713b3f66b85d1f783
alishaikh04 <alishaikhzk@gmail.com>
2025-09-07 13:49:34 +0500

    Update Main.java

--- Shallow copy/Main.java
+++ Shallow copy/Main.java
@@ -1,26 +1,8 @@
-
-public class Main {
-   public void run(){
-     
-    try {
-        Array a1 =  new Array();
-        Array a2 =  (Array) a1.clone();
-
-        System.out.println("---------ORGINAL ONE----------");
-        System.out.println("VALUES FORM a1 IS :" + a1);
-        System.out.println("----------SHALLOW COPY-----------");
-        System.out.println("VALUES FORM COPY IS: " + a2);
-
-        a2.increment();
-        System.out.println("==============VALUES AFTER INCREMENT===============");
-        System.out.println("---------ORGINAL ONE----------");
-        System.out.println("VALUES FORM a1 IS :" + a1);
-        System.out.println("----------SHALLOW COPY-----------");
-        System.out.println("VALUES FORM COPY IS: " + a2);
-
-
-    } catch (CloneNotSupportedException e) {
-        e.printStackTrace();
+public class Main {
+    public static void main(String[] args) {
+        App m1 =  new App();
+        m1.run();
     }
-   }    
 }
+
+    
