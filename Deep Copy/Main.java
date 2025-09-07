e777e1592aded47d86e1ccc346c29d3f8e570480
alishaikh04 <alishaikhzk@gmail.com>
2025-09-07 13:47:40 +0500

    Update Main.java

--- Deep Copy/Main.java
+++ Deep Copy/Main.java
@@ -1,29 +1,6 @@
 public class Main {
-    public void run(){
-         
-        try {
-            Array a1 =  new Array();
-            Array a2= (Array) a1.clone();
-
-            System.out.println("---------ORIGINAL ONE-------------");
-            System.out.println("Values of a1 Array : " + a1);
-              System.out.println("---------DEEP COPY-----------------");
-            System.out.println("Values of a2 Array : " + a2);
-
-            a1.increment();
-
-            System.out.println("After increment in Array : ");
-            System.out.println("---------ORIGINAL ONE-------------");
-            System.out.println("Now the value of a1 : " + a1);
-            System.out.println("---------DEEP COPY-----------------");
-            System.out.println("Now the value of a2 : " + a2);
-            
-
-
-
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        App m1 =  new App();
+        m1.run();
     }
-    
 }
